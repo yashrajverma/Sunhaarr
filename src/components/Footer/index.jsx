a8cb09faa586c00b2acaa6be3374f66512978240
yashrajverma <yashrajverma798@gmail.com>
2024-11-17 15:29:44 +0530

    CORS

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,12 +5,6 @@ import Icon from "../Icon"
 
 const navigation = {
     categories: categories.filter((item) => item.type === 'category'),
-    support: [
-        { name: 'Pricing', href: '#' },
-        { name: 'Documentation', href: '#' },
-        { name: 'Guides', href: '#' },
-        { name: 'API Status', href: '#' },
-    ],
     company: [
         { name: 'About', href: '#' },
         { name: 'Blog', href: '#' },
@@ -97,7 +91,7 @@ export default function Footer() {
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
                 <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
                     <div className="grid grid-cols-2 gap-8 xl:col-span-4">
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
+                        <div className="md:grid md:grid-cols-3 md:gap-8">
                             <div>
                                 <h3 className="text-sm font-bold text-primaryNavy tracking-wider uppercase">Categories</h3>
                                 <ul role="list" className="mt-4 space-y-4">
@@ -110,26 +104,12 @@ export default function Footer() {
                                     ))}
                                 </ul>
                             </div>
-                            <div className="mt-12 md:mt-0">
-                                <h3 className="text-sm font-bold text-primaryNavy tracking-wider uppercase">Support</h3>
-                                <ul role="list" className="mt-4 space-y-4">
-                                    {navigation.support.map((item) => (
-                                        <li key={item.name}>
-                                            <Link to={item.href} className="text-base text-gray-300 hover:text-white">
-                                                {item.name}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
                             <div>
                                 <h3 className="text-sm font-bold text-primaryNavy tracking-wider uppercase">Company</h3>
                                 <ul role="list" className="mt-4 space-y-4">
                                     {navigation.company.map((item) => (
                                         <li key={item.name}>
-                                            <Link to={item.href} className="text-base text-gray-300 hover:text-white">
+                                            <Link to={item.href} className="text-base text-primaryNavy hover:font-semibold">
                                                 {item.name}
                                             </Link>
                                         </li>
@@ -141,7 +121,7 @@ export default function Footer() {
                                 <ul role="list" className="mt-4 space-y-4">
                                     {navigation.legal.map((item) => (
                                         <li key={item.name}>
-                                            <Link to={item.href} className="text-base text-gray-300 hover:text-white">
+                                            <Link to={item.href} className="text-base text-primaryNavy hover:font-semibold">
                                                 {item.name}
                                             </Link>
                                         </li>
@@ -208,7 +188,7 @@ export default function Footer() {
                         <h3 className="text-sm font-bold text-primaryNavy tracking-wider uppercase">
                             Subscribe to our newsletter
                         </h3>
-                        <p className="mt-2 text-base text-gray-300">
+                        <p className="mt-2 text-base text-gray-800">
                             The latest news, articles, and resources, sent to your inbox weekly.
                         </p>
                     </div>
